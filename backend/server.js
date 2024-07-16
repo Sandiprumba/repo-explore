982782e6dbcba007862d540582d0d5cafa2a5295
sandiprumba <sandiprumba.lama@gmail.com>
2024-07-16 15:12:49 +0800

    package commit updated

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,13 @@ import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
-// import path from "path";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 9000;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
-// console.log(__dirname);
 //session manage data in web application its a middleware
 app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }));
 
@@ -25,19 +24,16 @@ app.use(passport.session());
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("connected ");
-});
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
 //frontend and baackend run in same port
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(9000, () => {
   console.log(`Server started on http://localhost:${PORT}`);
